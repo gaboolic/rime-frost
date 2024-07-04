ddd2222863e82db5a52147cb047a3021b6028fce
gaboolic <23441099@qq.com>
2024-07-04 18:28:52 +0800

    生成字频词频

--- others/program/reduce_freq_base_to_zhifreq.py
+++ others/program/reduce_freq_base_to_zhifreq.py
@@ -4,23 +4,30 @@ import math
 
 char_yin_freq_map = {}
 
-with open(os.path.join('', 'luna_pinyin.dict.yaml'), 'r', encoding='utf-8') as dict_file:
+def find_english_words(string):
+    pattern = r'[A-Za-z]+'  # 匹配由字母组成的单词
+    english_words = re.findall(pattern, string)
+    return english_words[0]
+
+with open(os.path.join('others', '多音字.txt'), 'r', encoding='utf-8') as dict_file:
     for line in dict_file:
         line = line.strip()
         if not '\t' in line or line.startswith("#"):
             continue
         params = line.split('\t')
-        if len(params) != 3:
+        if len(params) != 2:
             continue
         character = params[0]
-        encode = params[1]
-        freq = params[2]
+        encode_freq = params[1]
 
-        # todo 计算方法修改
-        freq = round(float(freq.strip('%')) / 100, 3)
-        print(line)
-        print(freq)
-        char_yin_freq_map[character+encode] = freq
+        encode_freq_params = encode_freq.split(";")
+        for encode_freq_param in encode_freq_params:
+            if encode_freq_param == "":
+                continue
+            encode = find_english_words(encode_freq_param)
+            freq = encode_freq_param[len(encode):]
+            char_yin_freq_map[character+encode] = float(freq)
+            
 
 word_freq_map = {}
 with open(os.path.join('others', '知频.txt'), 'r', encoding='utf-8') as dict_file:
@@ -53,8 +60,12 @@ for file_name in cn_dicts_common_list:
             if character in word_freq_map:
                 freq = int(word_freq_map[character])
             if character+encoding in char_yin_freq_map:
-                freq = freq * char_yin_freq_map[character+encode]
-            
+                freq = freq * char_yin_freq_map[character+encoding]
+                freq = math.ceil(freq)
+            if character == '合':
+                print(word_freq_map[character])
+                print(line)
+                print(f"{character}\t{encoding}\t{freq}\n")
             if freq > 0:
                 write_file.write(f"{character}\t{encoding}\t{freq}\n")
 
