5182b085283a19191f2606b24138d78521961937
gaboolic <23441099@qq.com>
2024-07-15 17:29:55 +0800

    去重

--- others/program/去重/deal_repeat.py
+++ others/program/去重/deal_repeat.py
@@ -2,8 +2,27 @@ import os
 import string
 
 
-jianpin_word_map = {}
+base_word_map = {}
 file_list = ['base.dict.yaml','ext.dict.yaml']
+for file_name in file_list:
+    file_name = os.path.join('cn_dicts', file_name)
+    with open(file_name, 'r') as file:
+        # 逐行读取文件内容
+        for line in file:
+            # 去除行尾的换行符
+            line = line.rstrip()
+            if line.startswith('#') or '\t' not in line:
+                continue
+            params = line.split("\t")
+            word = params[0]
+            encode = params[1]
+
+
+            key = word + encode
+            if key not in base_word_map:
+                base_word_map[key] = '1'
+
+print(len(base_word_map))
 
 # 使用 os 模块中的 listdir 函数列出指定文件夹中的所有文件和子目录
 file_names = os.listdir("cn_dicts_cell")
@@ -23,20 +42,7 @@ for file_name in file_names:
             word = params[0]
             encode = params[1]
             
-            pinyin = params[1]
-            shengmus = pinyin.split(" ")
-
-            word_freq = {}
-            word_freq["word"] = word
-            # word_freq["freq"] = freq
-
             key = word + encode
-            if key not in jianpin_word_map:
-                jianpin_word_map[key] = word_freq
-            else:
-
-                print("重复"+word)
-                print(jianpin_word_map[key])
-
-print(jianpin_word_map['什么shen me'])
+            if key in base_word_map:
+                print("重复:"+line)
 
