66033bc5016f704918f81c15079da3d88d6817a8
gaboolic <23441099@qq.com>
2024-07-15 19:48:23 +0800

    细胞词库去重

--- others/program/去重/deal_repeat.py
+++ others/program/去重/deal_repeat.py
@@ -1,9 +1,10 @@
 import os
 import string
+from collections import OrderedDict 
 
 
 base_word_map = {}
-file_list = ['base.dict.yaml','ext.dict.yaml']
+file_list = ['8105.dict.yaml','41448.dict.yaml','base.dict.yaml','ext.dict.yaml']
 for file_name in file_list:
     file_name = os.path.join('cn_dicts', file_name)
     with open(file_name, 'r') as file:
@@ -30,19 +31,28 @@ file_names = os.listdir("cn_dicts_cell")
 # 打印出所有找到的文件名
 for file_name in file_names:
     print(file_name)
-    file_name = os.path.join('cn_dicts_cell', file_name)
-    with open(file_name, 'r') as file:
+    read_file_name = os.path.join('cn_dicts_cell', file_name)
+
+    word_map = OrderedDict()
+    with open(read_file_name, 'r') as file:
         # 逐行读取文件内容
         for line in file:
             # 去除行尾的换行符
             line = line.rstrip()
             if line.startswith('#') or '\t' not in line:
+                word_map[line]=''
                 continue
             params = line.split("\t")
             word = params[0]
             encode = params[1]
             
             key = word + encode
-            if key in base_word_map:
-                print("重复:"+line)
+            if key not in base_word_map:
+                word_map[line]=''
+    
+    write_file_name = os.path.join('cn_dicts_cell', file_name)
+    write_file = open(write_file_name, 'w')
+
+    for word in word_map:
+        write_file.write(word+"\n")
 
