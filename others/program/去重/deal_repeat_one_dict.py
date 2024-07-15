edc1e2462a3f0e7d07232b8ecda89e5a1c4c6d29
gaboolic <23441099@qq.com>
2024-07-15 16:59:28 +0800

    细胞词库去重

--- others/program/去重/deal_repeat_one_dict.py
+++ others/program/去重/deal_repeat_one_dict.py
@@ -1,9 +1,8 @@
 import os
 import string
-
+from collections import OrderedDict 
 
 jianpin_word_map = {}
-file_list = ['base.dict.yaml','ext.dict.yaml']
 
 # 使用 os 模块中的 listdir 函数列出指定文件夹中的所有文件和子目录
 file_names = os.listdir("cn_dicts_cell")
@@ -11,22 +10,25 @@ file_names = os.listdir("cn_dicts_cell")
 # 打印出所有找到的文件名
 for file_name in file_names:
     print(file_name)
-    file_name = os.path.join('cn_dicts_cell', file_name)
+    read_file_name = os.path.join('cn_dicts_cell', file_name)
 
-    word_map = {}
-    with open(file_name, 'r') as file:
+    
+    word_map = OrderedDict()
+    with open(read_file_name, 'r') as file:
         # 逐行读取文件内容
         for line in file:
             # 去除行尾的换行符
             line = line.rstrip()
-            if line.startswith('#') or '\t' not in line:
-                continue
+            
             
             if line in word_map:
                 continue
             word_map[line]=''
     
+    write_file_name = os.path.join('cn_dicts_cell', file_name)
+    write_file = open(write_file_name, 'w')
+
     for word in word_map:
-        print(word)
+        write_file.write(word+"\n")
 
 
