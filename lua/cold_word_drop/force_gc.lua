4e615b0ddd0853565179276701aa07d523319f0f
gaboolic <23441099@qq.com>
2024-09-09 18:42:56 +0800

    force_gc报错修改

--- lua/cold_word_drop/force_gc.lua
+++ lua/cold_word_drop/force_gc.lua
@@ -1,7 +1,11 @@
 -- 暴力 GC
 -- 详情 https://github.com/hchunhui/librime-lua/issues/307
+-- collectgarbage()：默认调用，等同于 collectgarbage("collect")，触发完整的垃圾回收。
+-- collectgarbage("step")：执行垃圾回收的一小步。这个函数会返回一个布尔值，表示这一步是否完成了整个收集周期。
 -- 这样也不会导致卡顿，那就每次都调用一下吧，内存稳稳的
-function force_gc()
+local function force_gc()
     -- collectgarbage()
     collectgarbage("step")
 end
+
+return force_gc
